5dc5b73b6da0cce1e63fad088149a41358798e08
BKarret24 <dipirogov@edu.hse.ru>
2025-10-06 20:33:02 -0700

    Initial Commit

---
+++ dipirogovPW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dipirogovPW2
+//
+//  Created by Даниил Пирогов on 06.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ dipirogovPW2Tests/dipirogovPW2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  dipirogovPW2Tests.swift
+//  dipirogovPW2Tests
+//
+//  Created by Даниил Пирогов on 06.10.25.
+//
+
+import Testing
+@testable import dipirogovPW2
+
+struct dipirogovPW2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
