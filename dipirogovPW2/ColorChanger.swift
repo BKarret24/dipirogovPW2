7fd8118edd15c3b8391a02257e06b8cbf2464241
BKarret24 <dipirogov@edu.hse.ru>
2025-10-08 00:08:31 -0700

    Finish

--- dipirogovPW2/ColorChanger.swift
+++ dipirogovPW2/ColorChanger.swift
@@ -6,22 +6,23 @@ class ColorChanger {
         static let greenDefault: Double = 0.0
         static let blueDefault: Double = 0.0
         static let alphaDefault: Double = 1.0
+        static let randomRange: ClosedRange<Double> = 0...1
     }
-    var redComp: Double = Constants.redDefault
-    var greenComp: Double = Constants.greenDefault
-    var blueComp: Double = Constants.blueDefault
-    
+
+    var redComp: Double
+    var greenComp: Double
+    var blueComp: Double
+
     init(redComp: Double = Constants.redDefault, greenComp: Double = Constants.greenDefault, blueComp: Double = Constants.blueDefault) {
         self.redComp = redComp
         self.greenComp = greenComp
         self.blueComp = blueComp
     }
-    
+
     func getColor() -> UIColor {
-        let col = UIColor(red: self.redComp, green: self.greenComp, blue: self.blueComp, alpha: Constants.alphaDefault)
-        return col
+        UIColor(red: redComp, green: greenComp, blue: blueComp, alpha: Constants.alphaDefault)
     }
-    
+
     func changeRed(val: Double) {
         redComp = val
     }
@@ -34,5 +35,22 @@ class ColorChanger {
         blueComp = val
     }
     
-    
+    func setFromHex(_ hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return }
+
+        redComp = Double((rgb & 0xFF0000) >> 16) / 255
+        greenComp = Double((rgb & 0x00FF00) >> 8) / 255
+        blueComp = Double(rgb & 0x0000FF) / 255
+    }
+
+    func setRandom() {
+            redComp = Double.random(in: Constants.randomRange)
+            greenComp = Double.random(in: Constants.randomRange)
+            blueComp = Double.random(in: Constants.randomRange)
+        }
 }
+
--- dipirogovPW2/WishMakerViewController.swift
+++ dipirogovPW2/WishMakerViewController.swift
@@ -13,12 +13,22 @@ final class WishMakerViewController: UIViewController {
     static let red: String = "Red"
     static let green: String = "Green"
     static let blue: String = "Blue"
+    static let AnimDur = 0.3
     static let stackRadius: CGFloat = 20
     static let stackBottom: CGFloat = -40
     static let stackLeading: CGFloat = 20
+    static let stackColorTop: CGFloat = 120
+    static let stackSpacing: CGFloat = 10
+    static let stackColorBottom: CGFloat = 20
+    static let stackColorLeading: CGFloat = 20
+    static let stackColorTrailing: CGFloat = -20
     }
     
-    var backColor = ColorChanger()
+    private var backColor = ColorChanger()
+    private var sliderStack = UIStackView()
+    private let hexTextField = UITextField()
+    private let randomColorButton = UIButton(type: .system)
+        
     
     
     override func viewDidLoad() {
@@ -30,6 +40,8 @@ final class WishMakerViewController: UIViewController {
         ConfigureColor()
         ConfigureTitle()
         configureSliders()
+        configureHideButton()
+        configureColorTools()
     }
     
     private func ConfigureColor() {
@@ -70,6 +82,7 @@ final class WishMakerViewController: UIViewController {
     
     private func configureSliders() {
     let stack = UIStackView()
+    self.sliderStack = stack
     stack.translatesAutoresizingMaskIntoConstraints = false
     stack.axis = .vertical
     view.addSubview(stack)
@@ -101,4 +114,59 @@ final class WishMakerViewController: UIViewController {
         }
     }
     
+    private func configureHideButton() {
+            let button = UIButton(type: .system)
+            button.setTitle("Сместить слайдеры", for: .normal)
+            button.setTitleColor(.systemBlue, for: .normal)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.addTarget(self, action: #selector(hideSliders), for: .touchUpInside)
+            view.addSubview(button)
+            
+            NSLayoutConstraint.activate([
+                button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            ])
+        }
+        
+    @objc
+    private func hideSliders() {
+        UIView.animate(withDuration: Constants.AnimDur) {
+        self.sliderStack.isHidden.toggle()
+        }
+        }
+    
+    private func configureColorTools() {
+            hexTextField.placeholder = "HEX цвет"
+            hexTextField.borderStyle = .roundedRect
+            hexTextField.autocapitalizationType = .none
+            hexTextField.addTarget(self, action: #selector(applyHexColor), for: .editingDidEndOnExit)
+
+            randomColorButton.setTitle("Случайный цвет", for: .normal)
+            randomColorButton.addTarget(self, action: #selector(applyRandomColor), for: .touchUpInside)
+
+            let stack = UIStackView(arrangedSubviews: [hexTextField, randomColorButton])
+            stack.axis = .vertical
+            stack.spacing = Constants.stackSpacing
+            stack.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(stack)
+
+            NSLayoutConstraint.activate([
+                stack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Constants.stackColorTop),
+                stack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.stackColorLeading),
+                stack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: Constants.stackColorTrailing)
+            ])
+        }
+
+        @objc
+        private func applyHexColor() {
+            guard let text = hexTextField.text else { return }
+            backColor.setFromHex(text)
+            ConfigureColor()
+        }
+    
+        @objc
+        private func applyRandomColor() {
+            backColor.setRandom()
+            ConfigureColor()
+        }
 }
