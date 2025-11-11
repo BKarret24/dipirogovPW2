5dae538c949925f311006c6e13cb8fd5efd07246
BKarret24 <dipirogov@edu.hse.ru>
2025-11-11 00:41:12 -0800

    Point 3 (configured Table)

--- dipirogovPW2/WishStoringViewController.swift
+++ dipirogovPW2/WishStoringViewController.swift
@@ -1,8 +1,42 @@
 import UIKit
 
 final class WishStoringViewController: UIViewController {
+    
+    enum Constants {
+        static let tableCornerRadius: CGFloat = 10
+        static let tableOffSet: CGFloat = 40
+    }
+    
+    private let table : UITableView = UITableView(frame: .zero)
+
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
         view.backgroundColor = .cyan
+        configureTable()
+    }
+    
+    private func configureTable() {
+        view.addSubview(table)
+        table.backgroundColor = .lightGray
+        table.dataSource = self
+        table.separatorStyle = .none
+        table.layer.cornerRadius = Constants.tableCornerRadius
+        table.pin(to: view, Constants.tableOffSet)
+        
+        
+    }
+    
+}
+
+// MARK: - UITableViewDataSource
+extension WishStoringViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
 
